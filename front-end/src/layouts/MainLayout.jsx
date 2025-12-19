592ea51a542eb3f1f974fb1968ce2fa92693b70e
vinhthuan3108 <vinhthuan9@gmail.com>
2025-12-19 09:01:45 +0700

    fix xung đột

--- front-end/src/layouts/MainLayout.jsx
+++ front-end/src/layouts/MainLayout.jsx
@@ -1,10 +1,6 @@
 // src/layouts/MainLayout.jsx
 import { Outlet } from 'react-router-dom';
-<<<<<<< HEAD
-import Sidebar from '../components/common/Sidebar'; 
-=======
 import Header from '../components/common/Header';
->>>>>>> 3bf1dca90dc256d09f06edaa34fd4a30c01ef5d5
 import Footer from '../components/common/Footer';
 
 // function MainLayout() {
@@ -20,30 +16,6 @@ import Footer from '../components/common/Footer';
 // }
 function MainLayout() {
   return (
-<<<<<<< HEAD
-    <div style={{ display: 'flex', minHeight: '100vh' }}>
-      {/* 1. Thanh Menu bên trái (Sidebar) */}
-      <div style={{ width: '260px', flexShrink: 0 }}>
-        <Sidebar />
-      </div>
-
-      {/* 2. Vùng nội dung bên phải */}
-      <div style={{ 
-        flex: 1, 
-        display: 'flex', 
-        flexDirection: 'column',
-        backgroundColor: '#f4f7f6', // Màu nền nhẹ cho vùng admin
-        overflowX: 'hidden' 
-      }}>
-        {/* Phần nội dung trang */}
-        <main style={{ flex: 1, padding: '30px' }}>
-          <Outlet />
-        </main>
-
-        {/* Footer nằm dưới cùng của phần nội dung bên phải */}
-        <Footer />
-      </div>
-=======
     <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', fontFamily: 'sans-serif' }}>
       
       {/* Header luôn ở trên cùng */}
@@ -56,7 +28,6 @@ function MainLayout() {
 
       {/* Footer luôn ở dưới cùng */}
       <Footer />
->>>>>>> 3bf1dca90dc256d09f06edaa34fd4a30c01ef5d5
     </div>
   );
 }
